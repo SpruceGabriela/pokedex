773ceff2f595d475a01df0584d47e0fd77459504
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2021-08-08 13:57:52 -0300

    style: adjust global style

--- src/styles/GlobalStyle/index.tsx
+++ src/styles/GlobalStyle/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Reset from './_Reset';
 import ScrollBar from './_ScrollBar';
 
-const GlobalStyle = () => {
-  return (
-    <>
-      <Reset />
-      <ScrollBar />
-    </>
-  );
-};
+const GlobalStyle = () => (
+  <>
+    <Reset />
+    <ScrollBar />
+  </>
+);
 
 export default GlobalStyle;
