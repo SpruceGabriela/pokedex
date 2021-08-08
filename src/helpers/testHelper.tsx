437b673faf98db4a2962f3373b12591f83c65e09
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2021-08-08 13:54:17 -0300

    fix: adjust test helper

--- src/helpers/testHelper.tsx
+++ src/helpers/testHelper.tsx
@@ -4,10 +4,9 @@ import { render } from '@testing-library/react';
 import GlobalStyle from '../styles/GlobalStyle';
 import theme from '../styles/_theme';
 
-export const renderThemed = (children: any) =>
-  render(
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>,
-  );
+export const renderThemed = (children: any) => render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {children}
+  </ThemeProvider>,
+);
